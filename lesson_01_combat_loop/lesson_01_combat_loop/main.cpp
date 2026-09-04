462ad54bf47c368f4f4d7d7e4b9985976fc81a7b
heolmara <sewhitham@yahoo.com>
2026-09-04 10:19:26 +0100

    added rest of week 1 challenges

--- lesson_01_combat_loop/lesson_01_combat_loop/main.cpp
+++ lesson_01_combat_loop/lesson_01_combat_loop/main.cpp
@@ -7,43 +7,76 @@ int main()
 {
 	random_device rd; // set up generator
 	mt19937 gen(rd()); // seed mersenne twister (number generator)
-	uniform_int_distribution<> dist(10, 20); // gives number between 10 and 20 
+	uniform_int_distribution<> dist(5, 15); // gives number between 5 and 15
 
 	int playerHealth = 100;
 	int enemyHealth = 50;
 	int choice = 0;
+	int potionUses = 3;
+	string choiceQuit = "null";
+	bool quitGame = false;
 
-	while (playerHealth > 0 && enemyHealth > 0)
+	while (quitGame == false)
 	{
-		cout << "\nPlayer Health: " << playerHealth << endl;
-		cout << "Enemy Health: " << enemyHealth << endl;
-		cout << "1. Attack\n2. Defend\nChoose: ";
+		while (playerHealth > 0 && enemyHealth > 0)
+		{
+			cout << "\nPlayer Health: " << playerHealth << endl;
+			cout << "Enemy Health: " << enemyHealth << endl;
+			cout << "1. Attack\n2. Defend\n3. Heal (" << potionUses << " left)\nChoose: ";
+			cin >> choice;
+
+			if (choice == 1)
+			{
+				enemyHealth -= 10; // enemyHealth = enemyHealth - enemyHealth
+				cout << "You attack the enemy for 10 damage!\n";
+			}
+			else if (choice == 2)
+			{
+				cout << "You defend and reduce incoming damage!\n";
+			}
+			else if (choice == 3)
+			{
+				if (potionUses > 0)
+				{
+					potionUses -= 1;
+					playerHealth += 10;
+					cout << "You heal for 10 HP!\n";
+				}
+				else if (potionUses <= 0)
+				{
+					cout << "You have no more potions!\n";
+				}
+			}
+
+			if (enemyHealth > 0)
+			{
+				int enemyDamage = (choice == 2) ? 2 : dist(gen);
+				playerHealth -= enemyDamage;
+				cout << "The enemy attacks you for " << enemyDamage << " damage!\n";
+			}
+		}
+
+		cout << ((playerHealth <= 0) ? "You were defeated!\n" : "\nEnemy defeated!\n");
+		cout << "\nQuit Game?\n1. No\n2. Yes\nChoose: ";
 		cin >> choice;
 
 		if (choice == 1)
 		{
-			enemyHealth -= 10; // enemyHealth = enemyHealth - enemyHealth
-			cout << "You attack the enemy for 10 damage!\n";
+			cout << "Restarting!\n";
+			playerHealth = 100;
+			enemyHealth = 50;
 		}
 		else if (choice == 2)
 		{
-			cout << "You defend and reduce incoming damage!\n";
+			cout << "Quitting!\n";
+			quitGame = true;
 		}
-		else if (choice == 3)
+		else
 		{
-			playerHealth + 10;
-			cout << "You heal for 10 HP!\n";
+			cout << "Invalid choice, please try again.\n";
+			cin >> choice;
 		}
-		if (enemyHealth > 0)
-		{
-
-			int enemyDamage = (choice == 2) ? 2 : 5;
-			playerHealth -= enemyDamage;
-			cout << "The enemy attacks you for " << enemyDamage << " damage!\n";
-		}
-
 	}
 
-	cout << ((playerHealth <= 0) ? "You were defeated!\n" : "\nEnemy defeated!\n");
 	return 0;
 }
